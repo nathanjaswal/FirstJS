ddc0c5e3ff845bf6b689c2a5146533cd4eea6c8b
Nitin Jaswal <55303303+nathanjaswal@users.noreply.github.com>
2020-06-05 11:05:38 -0400

    First
    First

---
+++ happy1.js
@@ -0,0 +1,13 @@
+for (i=0; i<10; i++) {
+    //console.log("Hello!!! -> " + i);
+}
+
+
+var val = 0;
+var i = 1000;
+
+	for (i; i > 17; i-11) {
+  		val += i;
+        i -= 17;
+        console.log("Hello!!! -> " + i);
+	}
